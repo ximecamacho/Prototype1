7dbe4d4e80c914d59a2ec6a125d29456b657bcb5
Ximena Camacho <xcamacho@usc.edu>
2026-01-25 14:07:40 -0800

    Made Player + Floor
    player can move left and right, no jump yet. also no obstacles yet

---
+++ Game1_/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public InputAction MoveAction;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        MoveAction.Enable();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Vector2 move = MoveAction.ReadValue<Vector2>();
+      
+       Debug.Log(move);
+      
+       Vector2 position = (Vector2)transform.position + move * 0.01f;
+       
+       transform.position = position; 
+        
+    }
+}
