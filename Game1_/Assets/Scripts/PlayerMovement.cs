2db08b7fdc602dcc242628dcb02cd6778546d664
Ximena Camacho <xcamacho@usc.edu>
2026-01-25 14:25:30 -0800

    Player can move up now
    kinda stays going up if you hold it

--- Game1_/Assets/Scripts/PlayerMovement.cs
+++ Game1_/Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/*referenced https://learn.unity.com/course/2D-adventure-robot-repair/unit/game-environment-and-physics/tutorial/design-and-paint-your-game-tilemap?version=6.3
+for left and right movement
 
+*/
 public class PlayerMovement : MonoBehaviour
 {
     public InputAction MoveAction;
+  
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MoveAction.Enable();
+    
         
     }
 
@@ -17,9 +22,11 @@ public class PlayerMovement : MonoBehaviour
     {
        Vector2 move = MoveAction.ReadValue<Vector2>();
       
+      
        Debug.Log(move);
       
-       Vector2 position = (Vector2)transform.position + move * 0.01f;
+       Vector2 position = (Vector2)transform.position + move  * 0.01f;
+
        
        transform.position = position; 
         
