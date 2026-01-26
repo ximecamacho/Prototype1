c990b7211a9b869cd770207c085e2c84d5465d93
Ximena Camacho <xcamacho@usc.edu>
2026-01-25 16:59:46 -0800

    Jumpin

--- Game1_/Assets/Scripts/PlayerMovement.cs
+++ Game1_/Assets/Scripts/PlayerMovement.cs
@@ -1,34 +1,54 @@
+
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-/*referenced https://learn.unity.com/course/2D-adventure-robot-repair/unit/game-environment-and-physics/tutorial/design-and-paint-your-game-tilemap?version=6.3
-for left and right movement
+/*references:
+ https://learn.unity.com/course/2D-adventure-robot-repair/unit/game-environment-and-physics/tutorial/design-and-paint-your-game-tilemap?version=6.3
+-for left and right movement
+
+https://youtu.be/FO7auF7zXpU?si=7UI8rHodZFMC4DNJ
+-for jump
+
 
 */
 public class PlayerMovement : MonoBehaviour
 {
     public InputAction MoveAction;
-  
+
+    public InputAction JumpAction;
+    private float jumpForce = 3;
+    private Rigidbody2D rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         MoveAction.Enable();
-    
-        
+        JumpAction.Enable();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       Vector2 move = MoveAction.ReadValue<Vector2>();
-      
-      
-       Debug.Log(move);
-      
-       Vector2 position = (Vector2)transform.position + move  * 0.01f;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        if (JumpAction.IsPressed())
+        {
+            Jump();
+        }
+        Vector2 position_ = (Vector2)transform.position + move * 0.01f;
+        transform.position = position_;
+    }
 
-       
-       transform.position = position; 
-        
+    
+
+
+
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
+
     }
 }
