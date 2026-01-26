8b4a863cba8ed09a798f2be26268160f2ea5d0aa
Ximena Camacho <xcamacho@usc.edu>
2026-01-25 17:18:02 -0800

    Better Jump

--- Game1_/Assets/Scripts/PlayerMovement.cs
+++ Game1_/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ using UnityEngine.InputSystem;
 -for left and right movement
 
 https://youtu.be/FO7auF7zXpU?si=7UI8rHodZFMC4DNJ
+https://youtu.be/92DYkgBJBBo?si=OQK8xJwVJk-HmUNs
 -for jump
 
 
@@ -19,6 +20,8 @@ public class PlayerMovement : MonoBehaviour
     public InputAction JumpAction;
     private float jumpForce = 3;
     private Rigidbody2D rb;
+
+    private bool isGrounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -33,13 +36,20 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+    
+
         Vector2 move = MoveAction.ReadValue<Vector2>();
-        if (JumpAction.IsPressed())
+        if (JumpAction.IsInProgress() && GetIsGrounded())
         {
             Jump();
         }
         Vector2 position_ = (Vector2)transform.position + move * 0.01f;
         transform.position = position_;
+    }
+
+    private bool GetIsGrounded(){
+        return  Physics2D.Raycast(transform.position, Vector2.down, 1.5f, LayerMask.GetMask("Ground"));
+
     }
 
     
